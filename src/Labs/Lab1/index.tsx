cabae871238dee3eea73f56f3ee2f653da9e8130
Drumil23 <drumil.kotecha@gmail.com>
2025-07-09 05:55:35 +0530

    Added img src

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -1,6 +1,7 @@
 export default function Lab1() {
     return (
         <div>
+            <img src="images/NU_logo.png" alt="NU Logo" width={300} height="140px"/>
             <h2>Lab 1</h2>
             <p> Testing Lab 1!</p>
         </div>
